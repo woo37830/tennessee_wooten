a192cac8f2e85cf5bad72d04a5d7808a1ae20c21
John Wooten, Ph.D <jwooten@shoulderscorp.com>
2019-07-17 21:12:06 -0400

    Changed check_docker.sh to show running and not running

--- check_docker.sh
+++ check_docker.sh
@@ -5,8 +5,8 @@
 {
  ## will throw and error if the docker daemon is not running and jump
  ## to the next code chunk
- docker ps -q &> /dev/null
-# printf "Docker is \033[1;32mrunning.\033[0m\n";
+ docker ps -q &> /dev/null &&
+  printf "Docker is \033[1;32mrunning.\033[0m\n";
 } || {
   printf "Docker  is \033[1;31mnot running.\033[0m\n";
 }
