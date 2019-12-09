0d4d64ab42510124442a7ffc58d7c207c8cbbc62
woo37830 <jwooten37830@me.com>
2019-12-09 13:14:25 -0500

    Added monthly and weekly

---
+++ weekly_tasks.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+echo "Running from: " && echo "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+echo "Logs go to /var/log/weekly.out"
+echo "Remove weekly log files"
+
+rm -f /Users/woo/.freemind/log.*
+rm -f /Users/woo/.freemind/*.mm
+rm -f /Users/woo/Development/workspaces/freemind/*.log.*
+rm -f /Users/woo/Development/workspaces/freemind/*.log
+rm -f /Users/woo/Desktop/*.log
+rm -f /Applications/NetBeans/glassfish-3.1.2.2/glassfish/domains/domain1/logs/server.*
+
+echo "Do any weekly local mysql backups"
+if [ -e '/usr/local/bin/automysqlbackup' ]
+then
+ echo 'doing automysqlbackup'
+ /usr/local/bin/automysqlbackup /etc/automysqlbackup/myserver.conf
+else
+ echo '/usr/local/bin/automysqlbackup not found to do auto backup'
+fi
+
+if [ -e '/var/backup/db' ]
+then
+   chown -R root /var/backup/db
+   chgrp -R wheel /var/backup/db
+   chmod -R 755 /var/backup/db
+fi
+
+echo "All done with weekly_tasks.sh"
