53cf58e45c9cf4c8acc744e6149bd718c8b3e1de
woo37830 <jwooten37830@me.com>
2020-04-09 18:49:12 -0400

    Modified git_check and *_bill

--- personal_bill.sh
+++ personal_bill.sh
@@ -2,14 +2,13 @@
 #
 # Show the time charged to client for the current month
 # TODO - add extract the month and year from date and use.
-#                                                           
-# Usage patti_bill.sh mon-num (1-12)
+#
+# Usage personal_bill.sh mon-num (1-12)
 #
 if [ $# != 1 ];
 then
    echo "Usage: $0 mon-num (1-12)"
    exit
 fi
-
-~/bin/time_track_report.pl -client=patti -year=2020 -mon=$1
+~/bin/time_track_report.pl -client=personal -year=2020 -mon=$1
 #
