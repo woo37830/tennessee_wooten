22f0c24704d45c4f9a369ba6723aba3f8be9d31b
woo37830 <jwooten37830@me.com>
2020-06-28 19:58:00 -0400

    add pop, pushd, and dirs to ?

--- examples/arrayTest.php
+++ examples/arrayTest.php
@@ -1,15 +1,41 @@
 <?php
- function array_adder($v1, $v2)
- {
-    $result = array();
-    for(  $i=0;$i<min(count($v1),count($v2)); $i++)
-    {
+/********************
+  Add the elements at position i in one array to the element in the same position in the second array
+********************/
+function array_adder($v1, $v2, $offset=0)
+{
+   $result = array();
+   $size = max(count($v1),count($v2));
+   $short = min(count($v1), count($v2));
+   if( $offset != 0 ) {
+     for( $i=0; $i<$offset; $i++ ) {
+       $result[$i] = 0;
+     }
+   }
+   if( $size == $short ) {
+     for(  $i=$offset;$i<$size; $i++)
+     {
+        $result[$i] = $v1[$i] + $v2[$i];
+     }
+   } else {
+     for( $i=$offset;$i<$short; $i++) {
        $result[$i] = $v1[$i] + $v2[$i];
-    }
-    return $result;
- }
+     }
+     for( $i=$short; $i<$size; $i++ ) {
+       if( count($v1) < count($v2) ) {
+         $result[$i] = $v2[$i];
+       } else {
+         $result[$i] = $v1[$i];
+       }
+     }
+   }
+   return $result;
+};
  $v1 = array(1,2,3,4,5,6,7,8,9,10);
  $v2 = array(10,10,10,10,10,10,10,10,10,10);
  $v3 = array_adder($v1, $v2);
  var_dump($v3);
+ $v4 = array(1,2,3);
+ $v5 = array_adder($v1,$v4,3);
+ var_dump($v5);
 ?>
