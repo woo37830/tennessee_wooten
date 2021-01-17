a5dde94628728eaf0111b35696708310ab3698f8
woo37830 <jwooten37830@me.com>
2021-01-17 11:00:02 -0500

    Updated snappyMesh to include possibility of meta data with T, flow, etc boundary conditions

--- snappyMesh.sh
+++ snappyMesh.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 # prepare your STL files and edit this script to match their names and directories
 # command-line input
- 
-   if (( $# < 3 )) 
+
+   if (( $# < 5 ))
    then
-      echo 'Usage: snappyMesh.sh <dir-of-stls> <ouput.stl> <hex size [m]> [expand-factor]'
+      echo 'Usage: snappyMesh.sh <case_name> <dir-of-stls> <meta_data> <ouput.stl> <hex size [m]> [expand-factor]'
      exit 1
-    fi 
-    directory=$1
-    output=$2
-    cell_size=$3
- 
-    if (( $# > 3))
+    fi
+    $case=$1
+    $meta=$2
+    directory=$3
+    output=$4
+    cell_size=$5
+
+    if (( $# > 5))
        then
-        expand_factor=$4
+        expand_factor=$6
     else
         expand_factor=1.001
     fi
@@ -22,32 +24,43 @@
 echo  $2
 # snappyHexMesh reads from constant/triSurface
 mv $2 constant/triSurface
- 
+
 # create a background mesh from the prepared model
-python ~/bin/create_blockmesh.py constant/triSurface/impeller.stl 0.001 1.001
- 
+python ~/bin/create_blockmesh.py constant/triSurface/$output $cell_size $expand_factor
+
 # remove relics from old tries
 rm -r 1 2 3
- 
+
 # edges and other features; system/surfaceFeatureExtractDict must be defined
 surfaceFeatureExtract
- 
+
 # background mesh
 blockMesh
- 
+
+# remove output.stl used to create blockMesh
+rm -f constant/triSurface/$output
+
 # meshing in parallel: system/decomposeParDict must be defined;
 # use mpiexec on windows and mpirun on linux
 decomposePar -force
 mpiexec -np 4 snappyHexMesh -parallel
 reconstructParMesh -latestTime
- 
+
 # sets and zones, if there's anything in system/topoSetDict;
 # also, updates on patches if there's anything in system/changeDictionaryDict
 topoSet
 setsToZones
 changeDictionary
- 
+
 # check and see
 checkMesh
 paraFoam
 
+# convert output files to vtk for ParaView
+foamToVTK
+
+# zip up VTK and move to home directory
+
+tar -cf - VTK | gzip > ~/$case_VTK.tar.gz
+
+echo "All Done!"
