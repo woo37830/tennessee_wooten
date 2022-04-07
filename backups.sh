4e839024bd6aa04f36774a39fe84a2f234abea6b
woo37830 <jwooten37830@me.com>
2022-04-07 17:31:13 -0400

    Check for backups before listing

--- backups.sh
+++ backups.sh
@@ -1,7 +1,11 @@
+if [ -e /var/backup/db/daily ]; then
 echo "daily"
 ls -al /var/backup/db/daily/mm_prod | grep -E 'daily' | awk '{print $9}'
 echo "weekly"
 ls -al /var/backup/db/weekly/mm_prod | grep -E 'weekly' | awk '{print $9}'
 echo "monthly"
 ls -al /var/backup/db/monthly/mm_prod | grep -E 'monthly' | awk '{print $9}'
+else
+    echo "backups not present"
+fi
 echo "All Done!"
