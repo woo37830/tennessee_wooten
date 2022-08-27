e4581b47a14de9c255cdcd76a21afc0e5f7e4e1c
woo37830 <jwooten37830@me.com>
2022-08-27 14:48:00 -0400

    Copied from woo-va-air and pushing

--- mysql_monitor.sh
+++ mysql_monitor.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
+#br="$(tput -S <<<$'setaf 1\nbold\n')" # Bold Red
+br="\033[1;31m";
+#bg="$(tput -S <<<$'setaf 2\nbold\n')" # Bold Green
+bg="\033[1;32m";
+#ar="$(tput sgr0)" # Text attributes reset
+ar="\033[0m ";
+FILE=/usr/local/mysql/bin/mysqld
+APP=MySQL
+if [ -e $FILE ]; then
 UP=$(pgrep mysqld | wc -l);
-if [ $UP != 1 ];
-then
-   printf "MYSQL is \033[1;31mdown.\033[0m\n";
+  if [ $UP != 1 ];
+    then
+       printf "${APP} is ${br}not running.${ar}\n";
+    else
+       printf "${APP} is ${bg}running.${ar}\n";
+  fi
 else
-   printf "MySQL is \033[32mrunning.\033[0m\n";
-fi
-#printf "`csrutil status | grep --color 'disabled'`\n";
-if  csrutil status | grep 'disabled' &> /dev/null; then
-printf "System Integrity Protection status: \033[1;31mdisabled\033[0m\n";
-else
-printf "System Integrity Protection status: \033[32menabled\033[0m\n";
+  printf "${APP} is ${bg}not available.${ar}\n";
 fi
